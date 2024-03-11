7ce85c7559bf9774edbdcc6a249c8d625a940992
Hasan Can Demirci <hasancandemirci5341@gmail.com>
2024-03-11 14:08:37 +0300

    box_plot done

--- R_os_box_plot_ violin_plot.R
+++ R_os_box_plot_ violin_plot.R
@@ -17,7 +17,7 @@ library(ggpubr) #ggviolin ve ggboxplot fonksiyonlari icin
 
 ## Data Import ##
         
-data <- read_xlsx("", sheet = "")
+data <- read_xlsx("data_source.xlsx", sheet = "BoxPlot")
 
 ## Data Toparlama / Duzenleme ##
 
@@ -25,34 +25,34 @@ colnames(data)
 
 data <- data %>%
   pivot_longer(
-    cols = c(),
-    names_to = "",
-    values_to = "")
+    cols = c("A_Spor", "B_Spor", "C_Spor", "D_Spor", "E_Spor"),
+    names_to = "kanal",
+    values_to = "reyting")
 
 
 ## Tek Renkli Box Plot, Legend Olmadan
 
-ggplot(data, aes(x=, y=, group=)) +
-  geom_boxplot(fill="", notch=FALSE, outlier.shape = NA) +
-  geom_jitter(size=1, color="", width=0.1) +
+ggplot(data, aes(x=kanal, y=reyting, group=kanal)) +
+  geom_boxplot(fill="lightblue", notch=FALSE, outlier.shape = NA) +
+  geom_jitter(size=1, color="black", width=0.1) +
   theme_ipsum()
 
 
 ## Ozel Renkli Box Plot ##
 
 
-ggboxplot(data, x = "", y = "", fill = "",
-          palette = c("red",....),
-          add = "boxplot", add.params = list(fill = rep(c("red",....)))) 
+ggboxplot(data, x = "kanal", y = "reyting", fill = "kanal",
+          palette = c("red","blue", "orange", "lightblue","purple"),
+          add = "boxplot", add.params = list(fill = rep(c("red","blue", "orange", "lightblue","purple")))) 
 
 
 ## Ozel Renkli Violin Plot ##
 
-ggviolin(data, x = "", y = "", fill = "",
-         palette = c("red",....),
-         add = "boxplot", add.params = list(fill = "white"))
+ggviolin(data, x = "kanal", y = "reyting", fill = "kanal",
+         palette = c("red","blue", "orange", "lightblue","purple"),
+         add = "boxplot", add.params = list(fill = "white"))+
+         theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 
 
 
-#theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
